7c747be5768358d01b7b98c74921dc822e5ec863
SIMONE MUSSINI <284900@studenti.unimore.it>
2025-02-08 17:42:03 +0100

    Add CRUD operations for helmets in the helmets controller and update routes

--- controllers/helmets_controller.go
+++ controllers/helmets_controller.go
@@ -5,6 +5,7 @@ import (
 	"safecap_backend/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetAllHelmets(c *gin.Context) {
@@ -52,4 +53,67 @@ func GetHelmetId(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"helmet_id": helmet.ID})
+}
+
+func UpdateHelmet(c *gin.Context) {
+	helmetId := c.Param("helmet-id")
+	var helmet models.Helmet
+
+	if err := db.First(&helmet, helmetId).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Helmet not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	if err := c.ShouldBindJSON(&helmet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := db.Save(&helmet).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, helmet)
+}
+
+func CreateHelmet(c *gin.Context) {
+	var helmet models.Helmet
+
+	if err := c.ShouldBindJSON(&helmet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := db.Create(&helmet).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, helmet)
+}
+
+func DeleteHelmet(c *gin.Context) {
+	helmetId := c.Param("worker-id")
+	var helmet models.Helmet
+
+	if err := db.First(&helmet, helmetId).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Helmet not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	if err := db.Delete(&helmet).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Helmet deleted"})
 }
--- routes/helmets_routes.go
+++ routes/helmets_routes.go
@@ -10,4 +10,10 @@ func RouteHelmets(r *gin.RouterGroup) {
 	r.GET("", controllers.GetAllHelmets)
 	r.GET("/:helmet-id", controllers.GetHelmetDetails)
 	r.GET("UUID/:helmet-uuid", controllers.GetHelmetId)
+
+	r.PUT("/:helmet-id", controllers.UpdateHelmet)
+
+	r.POST("", controllers.CreateHelmet)
+
+	r.DELETE("/:helmet-id", controllers.DeleteHelmet)
 }
