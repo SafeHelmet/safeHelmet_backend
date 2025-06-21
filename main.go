66020e181867ca12dbfc655606ce649d593336b1
paridestomeo <166159431+paridestomeo@users.noreply.github.com>
2025-06-21 16:35:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-	"log"
-	"os"
-	"safecap_backend/API"
-	"safecap_backend/config"
-	"safecap_backend/controllers"
-	"safecap_backend/routes"
-	"safecap_backend/utils"
+"log"
+"os"
+"safecap_backend/API"
+"safecap_backend/config"
+"safecap_backend/controllers"
+"safecap_backend/routes"
+"safecap_backend/utils"
+"safecap_backend/models"
+
 )
 
 func main() {
-	toseed := "false"
+db := config.ConnectToDB(config.GetDSN())
+config.Migrate(db)
+// TODO scelta discutibile
+controllers.InitDatabase(db)
 
-	// Verifica se è stato passato un argomento da riga di comando
-	if len(os.Args) >= 2 {
-		toseed = os.Args[1]
-	}
 
-	db := config.ConnectToDB(config.GetDSN())
-	config.Migrate(db)
-	// TODO scelta discutibile
-	controllers.InitDatabase(db)
+var helmets []models.HelmetCategory
+var count int
+if err := db.Find(&helmets).Count(&count).Error; err != nil {
+log.Println("Database need to be seeded!")
+utils.DeleteTables(db)
+err := utils.SeedDatabase(db)
+if err != nil {
+log.Fatal("Failed to seed database:", err)
+}
+log.Println("Database seeded successfully")
+}
 
-	if toseed == "true" {
-		utils.DeleteTables(db)
-		err := utils.SeedDatabase(db)
-		if err != nil {
-			log.Fatal("Failed to seed database:", err)
-		}
-		log.Println("Database seeded successfully")
-	}
+// Avvia il scheduler per la chiamata API in una goroutine
+go API.StartAPICallScheduler(db)
 
-	// Avvia il scheduler per la chiamata API in una goroutine
-	go API.StartAPICallScheduler(db)
+// Crea un router Gin
+r := config.SetupRouter()
+routes.DeclareRoutes(r)
 
-	// Crea un router Gin
-	r := config.SetupRouter()
-	routes.DeclareRoutes(r)
-
-	// Avvia il server sulla porta 8080
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal(err)
-	}
+// Avvia il server sulla porta 8080
+if err := r.Run(":8080"); err != nil {
+log.Fatal(err)
+}
 }
